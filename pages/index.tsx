4c64051b1834fad3ba7883d25d015a2bd5146b2e
Bross-tech <trustedodds100@gmail.com>
2025-09-11 15:46:04 +0000

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to DATA STORE 4GH 🇬🇭⚡</h1>
+      <p>Your go-to platform for data services in Ghana!</p>
+    </main>
+  );
+}
