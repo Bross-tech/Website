9ce66c02e8543d1449dd1ed91ed67c589f42c31f
Bross-tech <trustedodds100@gmail.com>
2025-09-16 11:11:30 +0000

    Delete components/WhatsAppWidget.tsx

--- components/WhatsAppWidget.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./WhatsAppChatWidget.css";
-
-const WhatsAppWidget = () => (
-  <div className="whatsapp-widget">WhatsApp Widget works!</div>
-);
-export { WhatsAppWidget };
